7e9c8fa4719ef52a024e27c9e773ccf323b47862
Ka666aH <dmitryhoroshev05@mail.ru>
2026-01-16 10:32:30 +0300

    Подключить БД.

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,9 +1,18 @@
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the container
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddDbContext<PostgreDBContext>
+    (
+    options =>
+    {
+        var connectionString = builder.Configuration.GetConnectionString("PostgreSQL") ?? Environment.GetEnvironmentVariable("CONNECTION_STRING");
+        options.UseNpgsql(connectionString);
+    }
+    );
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
