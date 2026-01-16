9199118cc63b6dc9d458c5777140adeccb55cb93
Ka666aH <dmitryhoroshev05@mail.ru>
2026-01-16 09:57:03 +0300

    Реализовать PostgreDBContext.

---
+++ Infrastructure/PostgreDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class PostgreDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Module> Modules { get; set; }
+        public DbSet<Assignment> Assignments { get; set; }
+        public DbSet<Submission> Submissions { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+    }
+}
