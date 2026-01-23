a03e386f79b4f915df761b076189c6f4b9301523
Ka666aH <dmitryhoroshev05@mail.ru>
2026-01-23 13:15:40 +0300

    Исправить интерфейсы.

--- Application/Interfaces/IAssignmentRepository.cs
+++ Application/Interfaces/IAssignmentRepository.cs
@@ -3,7 +3,7 @@ namespace Application.Interfaces
 {
     public interface IAssignmentRepository
     {
-        Task AddAssignmentAsync(Assignment assignment, Module module, CancellationToken ct = default);
+        Task<bool> AddAssignmentAsync(Assignment assignment, Guid moduleId, CancellationToken ct = default);
         Task<Assignment?> GetAssignmentByIdAsync(Guid assignmentId, CancellationToken ct = default);
         Task<List<Assignment>> GetAssignmentsByModuleIdAsync(Guid moduleId, CancellationToken ct = default);
         Task<bool> UpdateAssignmentTitleAsync(Guid assignmentId,string newTitle, CancellationToken ct = default);
--- Application/Interfaces/IEnrollmentRepository.cs
+++ Application/Interfaces/IEnrollmentRepository.cs
@@ -4,7 +4,8 @@ namespace Application.Interfaces
     public interface IEnrollmentRepository
     {
         Task AddEnrollmentAsync(Enrollment enrollment, CancellationToken ct = default);
-        Task<Enrollment?> GetEnrollmentByIdAsync(Guid enrollmentId, CancellationToken ct = default);
-        Task<bool> DeleteEnrollmentByIdAsync(Guid enrollmentId, CancellationToken ct = default);
+        Task<List<Enrollment>> GetEnrollmentByUserIdAsync(Guid userId, CancellationToken ct = default);
+        Task<List<Enrollment>> GetEnrollmentByCourseIdAsync(Guid courseId, CancellationToken ct = default);
+        Task<bool> DeleteEnrollmentByIdAsync(Guid userId, Guid courseId, CancellationToken ct = default);
     }
 }
